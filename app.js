e9727b44f0bf4a96ecedd41154ad3a8ec088f9dd
anushree1212 <anushreeph1274@gmail.com>
2025-01-08 16:05:44 +0530

    appjs

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 // Sample route
 app.get('/', (req, res) => {
-  res.send('Happiness is not by chance, but by choice. So value your every moments !');
+  res.send('Happiness is not by chance, but by choice. So value your every moments and cherish the memories !');
 });
 
 // Server listening
